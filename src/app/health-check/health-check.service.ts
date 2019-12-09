8ed03bfba360fdda5106fb5b579e08aa93e7e638
Gauri <gauri.datey2018@gmail.com>
2019-12-09 10:05:03 +0530

    environment related changes

--- src/app/health-check/health-check.service.ts
+++ src/app/health-check/health-check.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HealthCheckService {
-  HEALTHCHECK_URL = 'http://localhost:3000';
   constructor(private httpClient: HttpClient) {}
 
   getServerHealthCheck(): Observable<any> {
-    return this.httpClient.get<any>(this.HEALTHCHECK_URL + '/healthcheck');
+    return this.httpClient.get<any>(
+      environment.WEBSERVICE_URL + '/healthcheck'
+    );
   }
 }
